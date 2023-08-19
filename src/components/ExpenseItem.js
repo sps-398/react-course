c400a71336f625fec97208b6dd797ede1707611f
sps-398 <shashankpratapsingh5004@gmail.com>
2023-08-19 19:01:48 +0530

    Initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,32 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>
+                <div>
+                    Food
+                </div>
+                <div>
+                    $50
+                </div>
+            </div>
+            <div>
+                <div>
+                    Petrol
+                </div>
+                <div>
+                    $100
+                </div>
+            </div>
+            <div>
+                <div>
+                    Movies
+                </div>
+                <div>
+                    $200
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem
