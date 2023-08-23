f8a53de28e01fd6237226366047a79117dfb657a
sps-398 <shashankpratapsingh5004@gmail.com>
2023-08-23 14:30:33 +0530

    task2_ass2_react

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 
 function ExpenseItem(props) {
+    const [ amount, setAmount ] = useState(props.amount);
     function deleteExpense() {
         document.getElementById(props.id).remove();   
+    }
+    function changeAmount() {
+        console.log('clicked!');
+        setAmount('$100');
     }
     return (
         <div>
             <div id={props.id} className="expense_item">
                 <ExpenseDate date={props.date}></ExpenseDate>
-                <ExpenseDetails id={props.id} title={props.title} location={props.location} amount={props.amount} />
+                <ExpenseDetails id={props.id} title={props.title} location={props.location} amount={amount} />
                 <button onClick={deleteExpense}>Delete expense</button>
+                <button onClick={changeAmount}>Change amount</button>
             </div>
         </div>
     );
