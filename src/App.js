2d6054e63c847f6b536e295247a07655f8f78a8b
sps-398 <shashankpratapsingh5004@gmail.com>
2023-08-23 12:30:32 +0530

    task1_ass2_react

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location: 'Chennai'
-    },
-    { id: 'e2',
-    title: 'New TV',
-    amount: 799.49,
-    date: new Date(2021, 2, 12),
-    location: 'Noida'
-  },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      location: 'Mumbai'
-    }
-  ];
-
   return (
     <div>
       <h2>Let's get started!</h2>
-      <div id="expense-container">
-      </div>
-      <ExpenseItem id={expenses[1].id} title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} location={expenses[1].location}></ExpenseItem>
-      <ExpenseItem id={expenses[2].id} title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} location={expenses[2].location}></ExpenseItem>
+      <Expenses></Expenses>
     </div>
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,12 +2,16 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 
-function ExpenseItem(props) {   
+function ExpenseItem(props) {
+    function deleteExpense() {
+        document.getElementById(props.id).remove();   
+    }
     return (
         <div>
-            <div className="expense_item">
+            <div id={props.id} className="expense_item">
                 <ExpenseDate date={props.date}></ExpenseDate>
                 <ExpenseDetails id={props.id} title={props.title} location={props.location} amount={props.amount} />
+                <button onClick={deleteExpense}>Delete expense</button>
             </div>
         </div>
     );
