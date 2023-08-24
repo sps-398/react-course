762f25cae7d62a3fc9a9c792c6fec6c9de332fb2
sps-398 <shashankpratapsingh5004@gmail.com>
2023-08-24 22:13:41 +0530

    task6_ass2_react

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -6,9 +6,18 @@ function ExpenseForm() {
     const [ enteredAmount, setAmount ] = useState("");
     const [ enteredDate, setDate ] = useState("");
 
+    function addExpense(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        for(const entry of formData.entries()) {
+            console.log(entry[0]);
+            console.log(entry[1]);
+        }
+    }
+
     return (
         <div className="add_expense_form_contianer flex">
-            <form className='add_expense_form flex'>
+            <form id="add_expense_form" onSubmit={addExpense} className='add_expense_form flex'>
                 <div className='expense_form_input'>
                     <label>Title</label>
                     <input type="text" onChange={e => setTitle(e.target.value)} value={enteredTitle} placeholder="Enter expense title" name="expense_title"></input>
