fd69e7abd24803f1ae5fe9661f45a2b4ced49413
sps-398 <shashankpratapsingh5004@gmail.com>
2023-08-27 22:23:39 +0530

    task12_ass2_react

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,34 +1,43 @@
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "../ExpensesFilter";
+import './Expenses.css';
 import React, { useState } from 'react';
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState('2020');
   const expenses = props.expenses;
+  const filteredExpenses = expenses
+    .filter(expense => expense.date.getFullYear().toString() === filteredYear);
 
-    function filterChangeHandler(selectedYear) {
-      setFilteredYear(selectedYear);
-    }
-
+  let expenseContent = filteredExpenses.map(expense => {
+    console.log(expense);
     return (
-        <div>
-          <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
-          {expenses
-            .filter(expense => expense.date.getFullYear().toString() === filteredYear)
-            .map(expense => {
-              console.log(expense);
-              return (
-                <ExpenseItem
-                  key={expense.id}
-                  title={expense.title}
-                  amount={expense.amount}
-                  date={expense.date}
-                  location={expense.location}
-                ></ExpenseItem>
-              ) 
-            })}
-        </div>
-    );
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        location={expense.location}
+      ></ExpenseItem>
+    )
+  });
+
+  if (filteredExpenses.length === 0)
+    expenseContent = <p className="user_alert">No Expenses to show!</p>
+
+  else if(filteredExpenses.length === 1)
+    expenseContent = <>{expenseContent}<p className="user_alert">Only one expense here, Please add more...</p></>
+
+  function filterChangeHandler(selectedYear) {
+    setFilteredYear(selectedYear);
+  }
+
+  return (
+    <div>
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
+      {expenseContent}
+    </div>
+  );
 }
 
 export default Expenses;
